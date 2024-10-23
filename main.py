9ae59d1e4d1e19353d7ff036cad1a3eafe2c534b
konnehh <konrad@mllr.me>
2024-10-23 10:43:47 +0200

    changed update and draw logic to group based

--- main.py
+++ main.py
@@ -14,15 +14,25 @@ def main():
     timer = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
 
     while True:
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
               return
-        player.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
         dt = timer.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from constants import *
 import pygame # type: ignore
 
 class Player(CircleShape):
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
